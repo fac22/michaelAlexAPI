56c6cbb9c3b2cf683ff2fe767a3a5ff235da649e
MJOW1999 <michael.jow.williams@gmail.com>
2021-08-08 20:05:47 +0100

    Create booster with card details

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
-
+// Linking both APIs through the multiverse id
 const sets = (code) => fetch(`https://api.magicthegathering.io/v1/sets/${code}/booster`)
-.then((response) => response.json())
+.then((response) => {
+    if(!response.ok) {
+        throw new Error("Not a valid set")
+    }
+    return response.json();
+})
 .then((data) => CardsID(data.cards))
 
-const arr = [];
-const CardsID = (card) => {
-    card.forEach(e => {
-        fetch(`https://api.scryfall.com/cards/multiverse/${e.multiverseid}`).then(response => response.json()).then((deck) => console.log(deck)).catch((console.error));
 
+
+const booster = document.querySelector("#newBooster");
+const CardsID = (card) => {
+    console.log(card.length)
+    card.forEach(e => {
+        fetch(`https://api.scryfall.com/cards/multiverse/${e.multiverseid}`)
+        .then(response => response.json())
+        // .then((deck) => console.log(deck))
+        .then((card) => {
+            console.log(card)
+            const image = document.createElement("img");
+            image.src = `${card.image_uris.normal}`;
+            image.alt = "";
+
+            const name = document.createElement("h2");
+            name.innerHTML = card.name.bold();
+
+            const type = document.createElement("h3");
+            type.innerHTML = card.type_line.bold();
+
+            const oracle = document.createElement("p");
+            oracle.textContent = card.oracle_text;
+
+            const flavor = document.createElement("p");
+            flavor.innerHTML = card.flavor_text.italics();
+
+            const price = document.createElement("p");
+            price.textContent = `$${card.prices.usd}`;
+            
+            booster.append(image, name, type, oracle, flavor, price);
+        })
+        .catch((console.error));
+
+        
     })
+    
 }
 
-// const CardsID = card => {
-//     const cards = document.querySelector("#new-div");
-//     card.forEach(e => {
-//         const id = document.createElement("p");
-//         id.innerText = `${e.id}`;
-//         cards.append(id)
-//     })
-// }
 
 
-sets("ktk");
-console.log(arr)
-// sets("ktk").then((deck) => console.log(deck)).catch((console.error));
-// console.log(setTimeout(id1, 5000))
-// const getCards = (card) => fetch(`https://api.scryfall.com/cards/${sets(code.cards[0].id)}`).then((response) => response.json())
-// console.log(getCards(sets("dom")))
-// fetch(`https://api.magicthegathering.io/v1/sets/${sky}/`)
-// .then((response) => response.json())
-// .then((data) => console.log(data));
 
 
-//fetch(`https://api.scryfall.com/cards/multiverse/${arr[0]}`).then((response) => (response.json));
+function createCardUsingTemplate(newCard){
+    const { name, type, oracle, flavor, price } = newCard;
+    const template = document.querySelector("#template");
+    const domFragment = template.content.cloneNode(true);
+    domFragment.querySelector("#card_name").textContent = name;
+    domFragment.querySelector("#card_type").textContent = type;
+    domFragment.querySelector("#card_oracle").textContent = oracle;
+    domFragment.querySelector("#card_flavor").textContent = flavor;
+    domFragment.querySelector("#card_price").textContent = price;
+
+    booster.appendChild(domFragment);
+}
+
+
+
+sets("dom");
