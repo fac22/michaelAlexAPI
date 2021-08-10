0b75c5fbf6fe1b14b8b19a492448c9e0e50d4402
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-08-10 15:19:27 +0100

    test complete

--- sort-testing/script.js
+++ sort-testing/script.js
@@ -1,69 +1,42 @@
-// Linking both APIs through the multiverse id
-const sets = (code) => fetch(`https://api.magicthegathering.io/v1/sets/${code}/booster`)
-.then((response) => {
-    if(!response.ok) {
-        throw new Error("Not a valid set")
-    }
-    return response.json();
-})
-.then((data) => CardsID(data.cards))
+// sort code
 
+const sort = document.querySelector("#sort");
+const sortForm = document.querySelector("#sortForm");
+const sortTestArea = document.querySelector("#sort-test");
 
+sortForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-const booster = document.querySelector("#newBooster");
-const CardsID = (card) => {
-    console.log(card.length)
-    card.forEach(e => {
-        fetch(`https://api.scryfall.com/cards/multiverse/${e.multiverseid}`)
-        .then(response => response.json())
-        // .then((deck) => console.log(deck))
-        .then((card) => {
-            console.log(card)
-            const image = document.createElement("img");
-            image.src = `${card.image_uris.normal}`;
-            image.alt = "";
+	let cards = Array.from(document.getElementsByClassName("card"));
 
-            const name = document.createElement("h2");
-            name.innerHTML = card.name.bold();
+	switch (sort.value) {
+		case "nameAZ":
+			cards.sort((a, b) => {
+				let nameA = a.getAttribute("data-name").toUpperCase();
+				let nameB = b.getAttribute("data-name").toUpperCase();
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+				return 0;
+			});
+			cards.forEach((e) => sortTestArea.appendChild(e));
+			break;
+		case "priceHiLo":
+			cards.sort((a, b) => {
+				return b.getAttribute("data-price") - a.getAttribute("data-price");
+			});
+			cards.forEach((e) => sortTestArea.appendChild(e));
+			break;
+		case "rarityMC":
+			cards.sort((a, b) => {
+				return b.getAttribute("data-rarity") - a.getAttribute("data-rarity");
+			});
+			cards.forEach((e) => sortTestArea.appendChild(e));
+			break;
+	}
 
-            const type = document.createElement("h3");
-            type.innerHTML = card.type_line.bold();
-
-            const oracle = document.createElement("p");
-            oracle.textContent = card.oracle_text;
-
-            const flavor = document.createElement("p");
-            flavor.innerHTML = card.flavor_text.italics();
-
-            const price = document.createElement("p");
-            price.textContent = `$${card.prices.usd}`;
-            
-            booster.append(image, name, type, oracle, flavor, price);
-        })
-        .catch((console.error));
-
-        
-    })
-    
-}
-
-
-
-
-
-function createCardUsingTemplate(newCard){
-    const { name, type, oracle, flavor, price } = newCard;
-    const template = document.querySelector("#template");
-    const domFragment = template.content.cloneNode(true);
-    domFragment.querySelector("#card_name").textContent = name;
-    domFragment.querySelector("#card_type").textContent = type;
-    domFragment.querySelector("#card_oracle").textContent = oracle;
-    domFragment.querySelector("#card_flavor").textContent = flavor;
-    domFragment.querySelector("#card_price").textContent = price;
-
-    booster.appendChild(domFragment);
-}
-
-
-
-sets("dom");
+	console.log(cards);
+});
